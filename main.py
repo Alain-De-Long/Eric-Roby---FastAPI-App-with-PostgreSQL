b04ce9aced069f122f9ad49d752769f01f4a48cb
Alain De Long <leholong007@gmail.com>
2026-05-31 09:13:35 +0700

    feat(question): implement POST /questions endpoint for structured question creation

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
+from typing import List, Annotated
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
-from typing import List, Annotated
+from sqlalchemy.orm import Session
 
-app=FastAPI()
+from core.config import settings
+from database.session import create_all_tables, db_dependency
+from models.quiz import Quesions, Choices
+from schemas.quiz import QuestionBase, ChoiceBase
 
-class ChoiceBase(BaseModel):
-    choice_text: str
-    is_correct: bool
-    
-class QuestionBase(BaseModel):
-    question_text: str
-    choices: List[ChoiceBase]
+create_all_tables()
+
+app = FastAPI()
+
+
+@app.post("/questions")
+async def create_questions(question: QuestionBase, db: db_dependency):
+    db_question = Quesions(question_text=question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+
+    for choice in question.choices:
+        db_choice = Choices(
+            choice_text=choice.choice_text,
+            is_correct=choice.is_correct,
+            question_id=db_question.id,
+        )
+
+        db.add(db_choice)
+    db.commit()
+
+
+# if __name__ == "__main__":
+    # print(settings.POSTGRES_DB)
+    # print(settings.POSTGRES_USER)
+    # print(settings.POSTGRES_PASSWORD)
+    # print(settings.DATABASE_URL)
