606ee451cf37c87bdf716bfe21e135eb47d0fede
Alain De Long <76836871+AlainDeLong2k@users.noreply.github.com>
2026-06-14 10:13:18 +0700

    test(api): implement database transaction rollback mechanism for integration tests (#17)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from src.core.config import settings
-from src.database.session import create_all_tables
+from src.database.session import SessionLocal, create_all_tables, engine, get_db
 from src.main import app
 
 from tests.helpers.quiz_api import QuizApiClient
@@ -14,12 +14,34 @@ def setup_integration_database():
     yield
 
 
-@pytest.fixture(scope="session")
-def integration_client():
-    yield TestClient(app)
+@pytest.fixture(scope="function")
+def integration_db_session():
+    connection = engine.connect()
+    transaction = connection.begin()
+
+    session = SessionLocal(bind=connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
+def integration_client(integration_db_session):
+    def _override_get_db():
+        yield integration_db_session
+
+    app.dependency_overrides[get_db] = _override_get_db
+
+    client = TestClient(app)
+    yield client
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture(scope="function")
 def integration_quiz_api(integration_client):
     base_url = f"{settings.ENDPOINT}{settings.API_PREFIX}"
     return QuizApiClient(client=integration_client, base_url=base_url)
