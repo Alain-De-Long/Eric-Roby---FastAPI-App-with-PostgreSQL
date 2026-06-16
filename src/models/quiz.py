a6142e947847e3fd6ae24e9aa8a22c2e33aff77b
Alain De Long <leholong007@gmail.com>
2026-06-16 21:09:08 +0700

    refactor(model): add database relationship to questions and choices table

--- src/models/quiz.py
+++ src/models/quiz.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database.session import Base
 
@@ -13,11 +13,17 @@ class Questions(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     question_text: Mapped[str] = mapped_column(String, index=True)
 
+    choices: Mapped[list["Choices"]] = relationship(
+        back_populates="questions", cascade="all, delete-orphan", lazy="selectin"
+    )
+
 
 class Choices(Base):
     __tablename__ = "choices"
 
-    id = Column(Integer, primary_key=True, index=True)
-    choice_text = Column(String, index=True)
-    is_correct = Column(Boolean, default=False)
-    question_id = Column(Integer, ForeignKey("questions.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    choice_text: Mapped[str] = mapped_column(String, index=True)
+    is_correct: Mapped[bool] = mapped_column(Boolean, default=False)
+    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
+
+    question: Mapped[Questions] = relationship(back_populates="choices")
